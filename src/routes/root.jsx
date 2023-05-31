d4deb2789f03ba939b0e0cb476137bdc792ecb8e
Tiago Gomes <tidgomes@gmail.com>
2023-05-31 12:10:58 -0300

    create an action function

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -60,3 +60,8 @@ export async function loader() {
   const contacts = await getContacts()
   return { contacts }
 }
+
+export async function action() {
+  const contact = await createContact()
+  return { contact }
+}
