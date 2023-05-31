5510bf727909493d89a4882896a47cf7702cc718
Tiago Gomes <tidgomes@gmail.com>
2023-05-31 12:10:41 -0300

    wrap the former form for button in the Form component from react-router-dom

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,5 @@
-import { useLoaderData, Outlet, Link } from 'react-router-dom'
-import { getContacts } from '../contacts'
+import { useLoaderData, Outlet, Link, Form } from 'react-router-dom'
+import { getContacts, createContact } from '../contacts'
 
 export default function Root() {
   const { contacts } = useLoaderData()
@@ -20,9 +20,9 @@ export default function Root() {
             <div id='search-spinner' aria-hidden hidden={true} />
             <div className='sr-only' aria-live='polite'></div>
           </form>
-          <form method='post'>
+          <Form method='post'>
             <button type='submit'>New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
