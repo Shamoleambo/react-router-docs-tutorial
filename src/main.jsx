8053ba862103f45e53b56a534b102ab5ca3d3a3b
Tiago Gomes <tidgomes@gmail.com>
2023-05-31 12:00:24 -0300

    apply useLoaderData to retrieve the contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/root'
+import Root, { loader as contactsLoader } from './routes/root'
 import Contact from './routes/contact'
 import ErrorPage from './error-page'
 import './index.css'
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: contactsLoader,
     children: [{ path: 'contacts/:contactId', element: <Contact /> }]
   }
 ])
